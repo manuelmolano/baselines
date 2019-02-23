6771e65cdb0048814646b041c7a6baa052aa8376
manu <me@linux.com>
2019-02-23 14:41:43 -0500

    PEP8

--- baselines/common/vec_env/dummy_vec_env.py
+++ baselines/common/vec_env/dummy_vec_env.py
@@ -3,6 +3,7 @@ from gym import spaces
 from . import VecEnv
 from .util import copy_obs_dict, dict_to_obs, obs_space_info
 
+
 class DummyVecEnv(VecEnv):
     """
     VecEnv that does runs multiple environments sequentially, that is,
@@ -18,13 +19,15 @@ class DummyVecEnv(VecEnv):
         """
         self.envs = [fn() for fn in env_fns]
         env = self.envs[0]
-        VecEnv.__init__(self, len(env_fns), env.observation_space, env.action_space)
+        VecEnv.__init__(self, len(env_fns), env.observation_space,
+                        env.action_space)
         obs_space = env.observation_space
         self.keys, shapes, dtypes = obs_space_info(obs_space)
 
-        self.buf_obs = { k: np.zeros((self.num_envs,) + tuple(shapes[k]), dtype=dtypes[k]) for k in self.keys }
+        self.buf_obs = {k: np.zeros((self.num_envs,) + tuple(shapes[k]),
+                                    dtype=dtypes[k]) for k in self.keys}
         self.buf_dones = np.zeros((self.num_envs,), dtype=np.bool)
-        self.buf_rews  = np.zeros((self.num_envs,), dtype=np.float32)
+        self.buf_rews = np.zeros((self.num_envs,), dtype=np.float32)
         self.buf_infos = [{} for _ in range(self.num_envs)]
         self.actions = None
         self.specs = [e.spec for e in self.envs]
@@ -40,7 +43,9 @@ class DummyVecEnv(VecEnv):
         if not listify:
             self.actions = actions
         else:
-            assert self.num_envs == 1, "actions {} is either not a list or has a wrong size - cannot match to {} environments".format(actions, self.num_envs)
+            assert self.num_envs == 1, "actions {} is either not a list or" +\
+                " has a wrong size - cannot match" +\
+                " to {} environments".format(actions, self.num_envs)
             self.actions = [actions]
 
     def step_wait(self):
@@ -49,12 +54,13 @@ class DummyVecEnv(VecEnv):
             if isinstance(self.envs[e].action_space, spaces.Discrete):
                 action = int(action)
 
-            obs, self.buf_rews[e], self.buf_dones[e], self.buf_infos[e] = self.envs[e].step(action)
+            obs, self.buf_rews[e], self.buf_dones[e], self.buf_infos[e] =\
+                self.envs[e].step(action)
             if self.buf_dones[e]:
                 obs = self.envs[e].reset()
             self._save_obs(e, obs)
-        return (self._obs_from_buf(), np.copy(self.buf_rews), np.copy(self.buf_dones),
-                self.buf_infos.copy())
+        return (self._obs_from_buf(), np.copy(self.buf_rews),
+                np.copy(self.buf_dones), self.buf_infos.copy())
 
     def reset(self):
         for e in range(self.num_envs):
